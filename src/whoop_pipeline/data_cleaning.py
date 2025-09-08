7a4cc5cfcccb38c8ec7374f6e219dc028508c8f7
JordanPickles <jordan.l.pickles@gmail.com>
2025-09-08 21:19:22 +0100

    feat(data ingest): adds data models and cleaning for workout, recovery and cycle

--- src/whoop_pipeline/data_cleaning.py
+++ src/whoop_pipeline/data_cleaning.py
@@ -5,16 +5,14 @@ class WhoopDataCleaner():
     def clean_sleep_data(self, df:pd.DataFrame) -> pd.DataFrame:
         """Cleans the sleep data DataFrame."""
         
-        # Renames all columns to replace . with _
-        df.columns = df.columns.str.replace('.', '_', regex=False)
+        df.columns = df.columns.str.split('score.').str[-1] # Keeps everything after score.
         # Convert date columns to datetime
         datetime_columns = ['created_at', 'updated_at', 'start', 'end']
         for col in datetime_columns: # Converts all date columns to datetime
             df[col] = pd.to_datetime(df[col])
 
-        
         df['timezone_offset'] = df['timezone_offset'].apply(self.tz_offset_to_minutes)
-
+        
         int_cols = [col for col in df.columns if col.endswith("milli") or col.endswith("count") or col.endswith("_id")] # lists all columns ending with milli to be converted to in
         for col in int_cols:
             df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0).astype(int)
@@ -25,15 +23,6 @@ class WhoopDataCleaner():
             if col.endswith("percentage"): #converts all percentages to a decimal
                 df[col] = df[col] / 100
         
-
-        # Handle missing values
-        df.fillna({
-            'score_respiratory_rate': df['score_respiratory_rate'].mean(),
-            'score_sleep_performance_percentage': df['score_sleep_performance_percentage'].mean(),
-            'score_sleep_consistency_percentage': df['score_sleep_consistency_percentage'].mean(),
-            'score_sleep_efficiency_percentage': df['score_sleep_efficiency_percentage'].mean()
-        }, inplace=True)
-                # Convert timezone_offset from "+01:00" format to minutes
         return df
         
     def tz_offset_to_minutes(self, offset_str):
@@ -43,3 +32,61 @@ class WhoopDataCleaner():
             sign = 1 if offset_str[0] == '+' else -1
             hours, minutes = map(int, offset_str[1:].split(':'))
         return sign * (hours * 60 + minutes)
+    
+    def clean_recovery_data(self, df:pd.DataFrame) -> pd.DataFrame:
+        """Cleans the recovery data DataFrame."""
+        
+        # Renames all columns to replace . with _
+        df.columns = df.columns.str.split('score.').str[-1] # Keeps everything after score.
+        # Convert date columns to datetime
+        datetime_columns = ['created_at', 'updated_at']
+        for col in datetime_columns:
+            df[col] = pd.to_datetime(df[col])
+        
+        df['cycle_id'] = pd.to_numeric(df['cycle_id']).astype(int)
+        df['sleep_id'] = df['sleep_id'].astype(str)
+
+        return df
+    
+    def clean_workout_data(self, df:pd.DataFrame) -> pd.DataFrame:
+        """Cleans the workout data DataFrame."""
+        
+        df.columns = df.columns.str.split('score.').str[-1] # Keeps everything after score.
+        # Convert date columns to datetime
+        datetime_columns = ['created_at', 'updated_at', 'start', 'end']
+        for col in datetime_columns:
+            df[col] = pd.to_datetime(df[col])
+        
+
+        int_cols = [col for col in df.columns if col in ['_id', 'rate', 'milli']]
+        for col in int_cols:
+            df[col] = pd.to_numeric(df[col], errors='coerce').astype(int)
+
+        float_cols = [col for col in df.columns if col in ['strain', 'kilojoule', 'meter']]
+        for col in float_cols:
+            df[col] = pd.to_numeric(df[col], errors='coerce').astype(float)
+        
+        return df
+
+
+    def clean_cycle_data(self, df:pd.DataFrame) -> pd.DataFrame:
+        """Cleans the cycle data DataFrame."""
+        
+        df.columns = df.columns.str.split('score.').str[-1] # Keeps everything after score.
+        # Convert date columns to datetime
+        datetime_columns = ['created_at', 'updated_at', 'start', 'end']
+        for col in datetime_columns:
+            df[col] = pd.to_datetime(df[col])
+
+        
+        int_cols = [col for col in df.columns if col in ['rate']]
+        for col in int_cols:
+            df[col] = pd.to_numeric(df[col], errors='coerce').astype(int)
+
+        
+        float_cols = [col for col in df.columns if col in ['strain', 'kilojoule']]
+        for col in float_cols:
+            df[col] = pd.to_numeric(df[col], errors='coerce').astype(float)
+        
+        
+        return df
--- src/whoop_pipeline/models.py
+++ src/whoop_pipeline/models.py
@@ -15,8 +15,8 @@ class Base(DeclarativeBase):
 
 class Sleep(Base):
     __tablename__ = 'fact_activity_sleep'
-    id: Mapped[str] = mapped_column(VARCHAR, primary_key=True)
-    cycle_id: Mapped[int]
+    sleep_id: Mapped[str] = mapped_column(VARCHAR, primary_key=True)
+    cycle_id: Mapped[int] = mapped_column(Integer, ForeignKey('fact_cycles.cycle_id'))
     v1_id: Mapped[int]
     user_id: Mapped[int]
     created_at: Mapped[DateTime]
@@ -25,31 +25,89 @@ class Sleep(Base):
     end: Mapped[DateTime]
     timezone_offset: Mapped[int]
     nap: Mapped[bool]
-    score_state: Mapped[str]
-    score_stage_summary_total_in_bed_time_milli: Mapped[int]
-    score_stage_summary_total_awake_time_milli: Mapped[int]
-    score_stage_summary_total_no_data_time_milli: Mapped[int]
-    score_stage_summary_total_light_sleep_time_milli: Mapped[int]
-    score_stage_summary_total_slow_wave_sleep_time_milli: Mapped[int]
-    score_stage_summary_total_rem_sleep_time_milli: Mapped[int]
-    score_stage_summary_sleep_cycle_count: Mapped[int]
-    score_stage_summary_disturbance_count: Mapped[int]
-    score_sleep_needed_baseline_milli: Mapped[int]
-    score_sleep_needed_need_from_sleep_debt_milli: Mapped[int]
-    score_sleep_needed_need_from_recent_strain_milli: Mapped[int]
-    score_sleep_needed_need_from_recent_nap_milli: Mapped[int]
-    score_respiratory_rate: Mapped[float]
-    score_sleep_performance_percentage: Mapped[float]
-    score_sleep_consistency_percentage: Mapped[float]
-    score_sleep_efficiency_percentage: Mapped[float]
+    state: Mapped[str]
+    stage_summary_total_in_bed_time_milli: Mapped[int]
+    stage_summary_total_awake_time_milli: Mapped[int]
+    stage_summary_total_no_data_time_milli: Mapped[int]
+    stage_summary_total_light_sleep_time_milli: Mapped[int]
+    stage_summary_total_slow_wave_sleep_time_milli: Mapped[int]
+    stage_summary_total_rem_sleep_time_milli: Mapped[int]
+    stage_summary_sleep_cycle_count: Mapped[int]
+    stage_summary_disturbance_count: Mapped[int]
+    sleep_needed_baseline_milli: Mapped[int]
+    sleep_needed_need_from_sleep_debt_milli: Mapped[int]
+    sleep_needed_need_from_recent_strain_milli: Mapped[int]
+    sleep_needed_need_from_recent_nap_milli: Mapped[int]
+    respiratory_rate: Mapped[float]
+    sleep_performance_percentage: Mapped[float]
+    sleep_consistency_percentage: Mapped[float]
+    sleep_efficiency_percentage: Mapped[float]
     
     
 
-# class Recovery(Base):
-#     __tablename__ = 'fact_recovery'
+class Recovery(Base):
+    __tablename__ = 'fact_recovery'
+    cycle_id: Mapped[int] = mapped_column(Integer, ForeignKey('fact_cycles.cycle_id'))
+    recovery_id: Mapped[str] = mapped_column(VARCHAR, primary_key=True)
+    user_id: Mapped[int]
+    created_at: Mapped[DateTime]
+    updated_at: Mapped[DateTime]
+    score_state: Mapped[str]
+    user_calibrating: Mapped[bool]
+    recovery_score: Mapped[int]
+    resting_heart_rate: Mapped[int]
+    hrv_rmssd_milli: Mapped[float]
+    spo2_percentage: Mapped[float]
+    skin_temp_celsius: Mapped[float]
 
  
+class Workout(Base):
+    __tablename__ = 'fact_workout'
+    workout_id: Mapped[str] = mapped_column(VARCHAR, primary_key=True)
+    v1_id: Mapped[int]
+    user_id: Mapped[int]
+    created_at: Mapped[DateTime]
+    updated_at: Mapped[DateTime]
+    start: Mapped[DateTime]
+    end: Mapped[DateTime]
+    timezone_offset: Mapped[DateTime]
+    sport_name: Mapped[str]
+    score_state: Mapped[str]
+    sport_id: Mapped[int]
+    strain: Mapped[float]
+    average_hear_rate: Mapped[int]
+    max_heart_rate: Mapped[int]
+    kilojoule: Mapped[float]
+    percent_recorded: Mapped[float]
+    altitude_gain_meter: Mapped[float]
+    altitude_change_meter: Mapped[float]
+    zone_zero_milli: Mapped[int]
+    zone_one_milli: Mapped[int]
+    zone_two_milli: Mapped[int]
+    zone_three_milli: Mapped[int]
+    zone_four_milli: Mapped[int]
+    zone_five_milli: Mapped[int]
 
-# class Workout(Base):
-#     __tablename__ = 'fact_workout'
 
+class Cycles(Base):
+    __tablename__ = 'fact_cycle'
+    cycle_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int]
+    user_id: Mapped[int]
+    created_at: Mapped[DateTime]
+    updated_at: Mapped[DateTime]
+    start: Mapped[DateTime]
+    end: Mapped[DateTime]
+    timezone_offset: Mapped[int]
+    score_state: Mapped[str]
+    strain: Mapped[float]
+    kilojoule: Mapped[float]
+    average_heart_rate: Mapped[int]
+    max_heart_rate: Mapped[int]
+
+
+    sleep_id: Mapped[Optional[str]] = mapped_column(VARCHAR, ForeignKey('fact_activity_sleep.sleep_id'))
+    recovery_id: Mapped[Optional[str]] = mapped_column(VARCHAR, ForeignKey('fact_recovery.recovery_id'))
+    
+    sleep: Mapped[Optional[Sleep]] = relationship("Sleep", backref="cycles")
+    recovery: Mapped[Optional[Recovery]] = relationship("Recovery", backref="cycles")
